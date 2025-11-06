349d38d4fedae609de6957f859dca12fff18b029
sgn08062 <sgn08062@gmail.com>
2025-11-06 17:39:46 +0900

    Initial Commit

---
+++ myFarm/src/test/java/com/example/myFarm/MyFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myFarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
